cc5b0b824231be17a065ced48f85c6c99468d9d2
Adam Alton <adamalton@gmail.com>
2021-02-13 15:34:40 +0000

    Try to fix USB temperature reading

--- djangoapp/djangoapp/utils/temperature.py
+++ djangoapp/djangoapp/utils/temperature.py
@@ -3,7 +3,12 @@ from temper import Temper
 
 def get_current_temperature():
     temper = Temper()
-    results: list[dict] = temper.read()
-    first = results[0]
-    temp = first["celsius"]
-    return temp
+    devices: list[dict] = temper.read()
+    for device in devices:
+        try:
+            return device["celsius"]
+        except KeyError:
+            continue
+    raise Exception(
+        f"None of the {len(devices)} devices read by Temper had a 'celsius' item. Data: {devices!r}"
+    )
