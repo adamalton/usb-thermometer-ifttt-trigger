bfd23a8261cc2e663a9db09196c23f3093c5f523
Adam Alton <adamalton@gmail.com>
2021-02-13 13:25:41 +0000

    Make the server available over the local network

--- bin/start_server.sh
+++ bin/start_server.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJECT_DIR=`dirname $DIR`
 
 source $PROJECT_DIR/.env/bin/activate
-nohup python $PROJECT_DIR/djangoapp/manage.py runserver 8085 &
+nohup python $PROJECT_DIR/djangoapp/manage.py runserver 0.0.0.0:8085 &
 
 DJANGO_SERVER_PROCESS_ID=$!
 
