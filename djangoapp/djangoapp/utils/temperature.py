f5e30365c3268b8d1832009bf095d20a3aaf65a9
Adam Alton <adamalton@gmail.com>
2021-02-13 15:41:02 +0000

    Fix temperature reading. Hopefully.

--- djangoapp/djangoapp/utils/temperature.py
+++ djangoapp/djangoapp/utils/temperature.py
@@ -4,11 +4,14 @@ from temper import Temper
 def get_current_temperature():
     temper = Temper()
     devices: list[dict] = temper.read()
+    keys = ["celsius", "internal temperature"]
     for device in devices:
-        try:
-            return device["celsius"]
-        except KeyError:
-            continue
+        for key in keys:
+            try:
+                return device[key]
+            except KeyError:
+                continue
     raise Exception(
-        f"None of the {len(devices)} devices read by Temper had a 'celsius' item. Data: {devices!r}"
+        f"None of the data dicts from the {len(devices)} devices read by Temper contained any of "
+        f"these keys: {''.join(keys)}. Data: {devices!r}"
     )
